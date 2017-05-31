ba18f4fc3aa94211feb5f67c44dff861553452c4
suretim1124 <suretim1124@gmail.com>
2017-05-31 23:49:21 +0800

    Add files via upload

---
+++ content.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class content : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["EMPY_ID"] == null)
+        {
+            Response.End();
+        }       
+    }
+}
